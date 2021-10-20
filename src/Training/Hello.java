a1fef8387169bbdd5cf157e40e110a5e5a32d507
murali <murali@murali-PC.attlocal.net>
2021-10-20 09:01:08 -0700

    second commit add hurray greets

--- src/Training/Hello.java
+++ src/Training/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the world of Java");
 		System.out.println("HI this is murali");
+		System.out.println("hurray my github repo is working");
 	}
 
 }
